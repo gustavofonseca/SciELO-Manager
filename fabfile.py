ee7fe3bb3241698f1cf48963260334df0ea0bd06
Gustavo Fonseca <gustavo@gfonseca.net>
2016-01-21 19:00:23 -0200

    nova versão do fabfile que vai substituir o Makefile

--- fabfile.py
+++ fabfile.py
@@ -1,90 +1,55 @@
 # coding: utf-8
-""" Tarefas para manutenção de instalações do SciELO Manager.
+import os
+import sys
+import tempfile
+from datetime import datetime
 
-Exemplo:
-    fab deploy:tag=v2016.01.18 --scielomanager_settings_file=/etc/scieloapps/scielomanager.py \
---installation_path=/var/www/manager_scielo_org
-"""
 from fabric.api import *
+from fabric.contrib import django
 
 
-SCIELOMANAGER_SETTINGS_FILE = env.get('scielomanager_settings_file',
-        '/etc/scieloapps/scielomanager.py')
-INSTALLATION_PATH = env.get('installation_path',
-        '/var/www/manager_scielo_org')
+TIME_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
 
 
-def get_version():
-    """ Obtém a versão ativa da instalação.
-
-    O formato da resposta segue a máscara:
-
-        <tag>-<total de commits além da tag>-<hash do head>
+# -------
+# Constantes passíveis de definição via argumentos.
+# -------
+TEMPDIR = env.get('tempdir', tempfile.gettempdir())
 
 
-    Veja https://www.git-scm.com/docs/git-describe
-    """
-    with cd(INSTALLATION_PATH):
-        output = run('git describe --long --dirty --abbrev=10 --tags')
-        return output
+# Açúcar sintático para a declaração e inicialização da variável
+# DJANGO_SETTINGS_FILE.
+django.project('scielomanager')
 
 
-def list_watchers():
-    """ `watcher` é uma entidade monitorada pelo Circus.
-
-    Cada `watcher` pode ser composto por 1 ou N processos.
-    """
-    with settings(sudo_user='root', warn_only=True):
-        output = sudo('circusctl status | grep scielomanager')
-        return [line.split(':')[0] for line in output.splitlines()]
-
-
-def reload_app():
-    """ Reinicia, com carinho, todos os `watchers` da aplicação.
-    """
-    with settings(sudo_user='root', warn_only=True):
-        for watcher in list_watchers():
-            sudo('circusctl reload %s' % watcher)
+# -------
+# O diretório base do projeto deve constar em `sys.path`, para que seja
+# possível importar seus pacotes e módulos.
+# -------
+HERE = os.path.abspath(os.path.dirname(__file__))
+PROJECT_BASE = HERE + '/scielomanager'
+if PROJECT_BASE not in sys.path:
+    sys.path.insert(0, PROJECT_BASE)
 
 
-def kill_circus():
-    """ Termina o Circus e todos os seus `watchers`.
-
-    Tendo em vista que o `circusd` é gerenciado pelo monitor do sistema operacional,
-    essa função funciona como um `reload` do tipo força-bruta e deve ser evitado
-    pois causa indisponibilidade da app.
-    """
-    with settings(sudo_user='root', warn_only=True):
-        sudo('circusctl quit')
+# -------
+# Partindo daqui é possível importar pacotes e módulos do projeto.
+# -------
+from django.conf import settings
+from journalmanager import models
 
 
-def deploy(tag):
-    """ Instala a versão `tag` da aplicação.
-    """
-    with settings(sudo_user='root', warn_only=True):
-        with settings(warn_only=True):
-            if run('test -d {path}'.format(path=INSTALLATION_PATH)).failed:
-                sudo('git clone https://github.com/scieloorg/scielo_publishing_schema.git {path}'.format(
-                    path=INSTALLATION_PATH))
+@task
+def dump_db():
+    db = settings.DATABASES['default']
+    dump_filename = '{dbname}-{datetime}.sql'.format(
+            dbname=db['NAME'], datetime=datetime.now().strftime(TIME_FORMAT))
+    dump_filepath = os.path.join(TEMPDIR, dump_filename)
 
-        with cd(INSTALLATION_PATH):
-            with prefix('workon scielomanager'):
-                sudo('git fetch origin && git checkout {tag} -b {tag}'.format(
-                    tag=tag))
-                sudo('SCIELOMANAGER_SETTINGS_FILE={path} make upgrade'.format(
-                    path=SCIELOMANAGER_SETTINGS_FILE))
+    cmd = 'pg_dump {dbname} -h {host} -p {port} -U {user} > {dump_filepath}'.format(
+        dbname=db['NAME'], host=db['HOST'], port=db['PORT'], user=db['USER'],
+        dump_filepath=dump_filepath)
 
-    reload_app()
+    local(cmd)
 
-
-def backup_db():
-    """ Realiza *dump* completo da base de dados da aplicação.
-    """
-    pass
-
-def restore_db(path_to_script):
-    """ Realiza *restore* da base de dados, com base no script SQL disponível em
-    `path_to_script`.
-    """
-    pass
-
+    return dump_filepath
